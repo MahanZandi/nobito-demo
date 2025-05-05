8de9a968051a45c0fc34a7591534f67b014df4d2
mahan <zandim221@gmail.com>
2025-05-06 01:48:18 +0330

    create doctot banner component

--- webapp/app/(website)/page.tsx
+++ webapp/app/(website)/page.tsx
@@ -3,29 +3,34 @@ import HomeSearchBox from "@/components/website/HomeSearchBox";
 import HomeStatistics from "@/components/website/HomeStatistics";
 import HomePopularSection from "@/components/website/HomePopularSection";
 import HomeConsultantsSection from "@/components/website/HomeConsultantsSection";
+import HomeDoctorsBanner from "@/components/website/HomeDoctorsBanner";
 
 const Home = () => {
-    return (
-        <section>
-            <HomeHeroSection/>
+  return (
+    <section className="overflow-x-hidden">
+      <HomeHeroSection />
 
-            <div className='pb-[72px]'>
-                <HomeSearchBox/>
-            </div>
+      <div className='pb-[72px]'>
+        <HomeSearchBox />
+      </div>
 
-            <div className='my-[72px]'>
-                <HomeStatistics/>
-            </div>
+      <div className='my-[72px]'>
+        <HomeStatistics />
+      </div>
 
-            <div className='py-[72px]'>
-                <HomePopularSection />
-            </div>
+      <div className='py-[72px]'>
+        <HomePopularSection />
+      </div>
 
-            <div className='py-[72px]'>
-                <HomeConsultantsSection />
-            </div>
-        </section>
-    );
-}
+      <div className='py-[72px]'>
+        <HomeConsultantsSection />
+      </div>
 
-export default Home
+      <div className='py-[72px]'>
+        <HomeDoctorsBanner />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
