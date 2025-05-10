531d51da4caf10575702e74398d8ae5cda7c7e76
mahan <zandim221@gmail.com>
2025-05-10 10:09:28 +0330

    added HomeTopMedicsSection component

--- webapp/app/(website)/page.tsx
+++ webapp/app/(website)/page.tsx
@@ -3,6 +3,7 @@ import HomeSearchBox from "@/components/website/HomeSearchBox";
 import HomeStatistics from "@/components/website/HomeStatistics";
 import HomePopularSection from "@/components/website/HomePopularSection";
 import HomeConsultantsSection from "@/components/website/HomeConsultantsSection";
+import HomeTopMedicsSection from "@/components/website/HomeTopMedicsSection";
 
 const Home = () => {
     return (
@@ -23,6 +24,10 @@ const Home = () => {
 
             <div className='py-[72px]'>
                 <HomeConsultantsSection />
+            </div>
+
+            <div className="py-[72px]">
+                <HomeTopMedicsSection />
             </div>
         </section>
     );
