a79e31006d34124c361efbc720aac401e7b1ed45
mahan <zandim221@gmail.com>
2025-05-16 21:28:05 +0330

    some change in mobile  horizen space

--- webapp/app/(website)/page.tsx
+++ webapp/app/(website)/page.tsx
@@ -46,7 +46,7 @@ const Home = () => {
         <HomeInsuranSection />
       </div>
 
-      <div className="py-[72px]">
+      <div className="py-[36px] xl:py-[72px]">
         <HomePatientFeedBack />
       </div>
     </section>
