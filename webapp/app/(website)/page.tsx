f1bff854e16f6ac96556111f7cae24962a92dc43
Mahan <zandim221@gmail.com>
2025-06-07 07:21:52 +0000

    Resolve "patient feedback slider"

--- webapp/app/(website)/page.tsx
+++ webapp/app/(website)/page.tsx
@@ -3,8 +3,10 @@ import HomeSearchBox from "@/components/website/HomeSearchBox";
 import HomeStatistics from "@/components/website/HomeStatistics";
 import HomePopularSection from "@/components/website/HomePopularSection";
 import HomeConsultantsSection from "@/components/website/HomeConsultantsSection";
+import HomePatientFeedBack from "@/components/website/HomePatientFeedBack";
 import HomeTopMedicsSection from "@/components/website/HomeTopMedicsSection";
 import HomeInsuranceBanner from "@/components/website/HomeInsuranBanner";
+import HomeInsuranSection from "@/components/website/HomeInsuranSection";
 import HomeDoctorsBanners from "@/components/website/HomeDoctorsBanners";
 
 const Home = () => {
@@ -38,6 +40,14 @@ const Home = () => {
 
       <div className="xl:pt-[168px] xl:pb-[72px] py-[36px]">
         <HomeInsuranceBanner/>
+      </div>
+
+      <div className="py-[36px] xl:py-[72px]">
+        <HomeInsuranSection />
+      </div>
+
+      <div className="py-[36px] xl:py-[72px]">
+        <HomePatientFeedBack />
       </div>
     </section>
   );
