f4056862268b34f3b833158ead0dff0f0efba1c2
mahan <zandim221@gmail.com>
2025-08-12 14:54:06 +0330

    completed hero section

---
+++ webapp/app/(website)/weblog/page.tsx
@@ -0,0 +1,13 @@
+import WeblogHeroSection from "@/components/website/WeblogHeroSection";
+
+const Weblog = () => {
+  return (
+    <>
+      <div className="pb-[36px] xl:pb-[72px]">
+        <WeblogHeroSection/>
+      </div>
+    </>
+  );
+};
+
+export default Weblog;
