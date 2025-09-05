d6b097221b15ca836d82d3dc3e6ef5c27e554a02
mahan <zandim221@gmail.com>
2025-09-05 17:42:56 +0330

    some change in SEO

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -12,14 +12,26 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Nobito | نوبیتو",
     description: "این پروژه یک دمو از وبسایت نوبت دهی آنلاین میباشد",
+    type: "website",
+    locale: "fa_IR",
+    url: "https://nobito-demo.vercel.app/",
     images: [
       {
-        url: "/favicon.ico",
+        url: "/logo-title.png",
         width: 1200,
         height: 630,
+        alt: "favicon",
       },
     ],
     siteName: "Nobito",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
   },
 };
 
