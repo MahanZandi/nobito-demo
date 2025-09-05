54458ccb16a2f23d3fbc8a06868f37cb445d2c59
mahan <zandim221@gmail.com>
2025-09-05 17:35:11 +0330

    added some metadata for simple seo

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,18 +1,36 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import "@/assets/styles/global.css";
+import { Metadata } from "next";
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
 }
 
-const Layout = ({children}: Props) => {
-    return (
-        <html>
-            <body dir='rtl' className="rtl:font-vazir-farsi">
-                {children}
-            </body>
-        </html>
-    )
-}
+export const metadata: Metadata = {
+  title: "Nobito | نوبیتو",
+  description: "این پروژه یک دمو از وبسایت نوبت دهی آنلاین میباشد",
+  openGraph: {
+    title: "Nobito | نوبیتو",
+    description: "این پروژه یک دمو از وبسایت نوبت دهی آنلاین میباشد",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    siteName: "Nobito",
+  },
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <html>
+      <body dir="rtl" className="rtl:font-vazir-farsi">
+        {children}
+      </body>
+    </html>
+  );
+};
 
 export default Layout;
