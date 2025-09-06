61ede5259ac0bde534baaa89ecc65702c3f00a25
mahan <zandim221@gmail.com>
2025-09-06 11:11:44 +0330

    change for PWA option

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import "@/assets/styles/global.css";
-import { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
+import Head from "next/head";
 
 interface Props {
   children: ReactNode;
@@ -9,6 +10,13 @@ interface Props {
 export const metadata: Metadata = {
   title: "Nobito | نوبیتو",
   description: "این پروژه یک دمو از وبسایت نوبت دهی آنلاین میباشد",
+  applicationName: "Nobito",
+  manifest: "/manifest.json", // ✅ connect manifest
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Nobito | نوبیتو",
+  },
   openGraph: {
     title: "Nobito | نوبیتو",
     description: "این پروژه یک دمو از وبسایت نوبت دهی آنلاین میباشد",
@@ -25,6 +33,12 @@ export const metadata: Metadata = {
     ],
     siteName: "Nobito",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Nobito | نوبیتو",
+    description: "این پروژه یک دمو از وبسایت نوبت دهی آنلاین میباشد",
+    images: ["/logo-title.png"],
+  },
   robots: {
     index: true,
     follow: true,
@@ -35,12 +49,20 @@ export const metadata: Metadata = {
   },
 };
 
+// ✅ رنگ بالای مرورگر (theme-color)
+export const viewport: Viewport = {
+  themeColor: "#ffffff",
+};
+
 const Layout = ({ children }: Props) => {
   return (
-    <html>
-      <body dir="rtl" className="rtl:font-vazir-farsi">
-        {children}
-      </body>
+    <html lang="fa" dir="rtl">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="apple-touch-icon" href="//favicon.ico" />
+      </Head>
+      <body className="rtl:font-vazir-farsi">{children}</body>
     </html>
   );
 };
